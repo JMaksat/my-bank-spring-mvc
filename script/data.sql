129f228f611ff322dbbf0fa6590667ad8a6f95c2
Marat Ergeshbaev <mergeshbaev@mils-s.ru>
2017-01-25 14:10:41 +0300

    Data structure and some data

--- script/data.sql
+++ script/data.sql
@@ -1,1 +1,84 @@
-insert
+insert into bank.directory (dir_id, dir_group, dir_type, description, date_created, date_modified, is_active, user_id)
+values (4, 'CONTACTS', 'PHONE_NUMBER', 'Phone number', '2016-01-01', '2016-01-01', true, 'scott');
+insert into bank.directory (dir_id, dir_group, dir_type, description, date_created, date_modified, is_active, user_id)
+values (5, 'CONTACTS', 'EMAIL', 'Email address', '2016-01-01', '2016-01-01', true, 'scott');
+insert into bank.directory (dir_id, dir_group, dir_type, description, date_created, date_modified, is_active, user_id)
+values (6, 'CONTACTS', 'SKYPE', 'Skype name', '2016-01-01', '2016-01-01', true, 'scott');
+insert into bank.directory (dir_id, dir_group, dir_type, description, date_created, date_modified, is_active, user_id)
+values (7, 'ADDRESS', 'ADDRESS_LINE1', 'First address line', '2016-01-01', '2016-01-01', true, 'scott');
+insert into bank.directory (dir_id, dir_group, dir_type, description, date_created, date_modified, is_active, user_id)
+values (8, 'CONTACTS', 'ADDRESS_LINE2', 'Second address line', '2016-01-01', '2016-01-01', true, 'scott');
+insert into bank.directory (dir_id, dir_group, dir_type, description, date_created, date_modified, is_active, user_id)
+values (9, 'OPERATIONS', 'INCOMING_TRANSFER', 'Transfer money from buffer account for incomings to personal account.', '2016-01-01', '2016-01-01', true, 'scott');
+insert into bank.directory (dir_id, dir_group, dir_type, description, date_created, date_modified, is_active, user_id)
+values (10, 'OPERATIONS', 'OUTGOING_TRANSFER', 'Transfer money from personal account to buffer account for outgoings.', '2016-01-01', '2016-01-01', true, 'scott');
+insert into bank.directory (dir_id, dir_group, dir_type, description, date_created, date_modified, is_active, user_id)
+values (11, 'OPERATIONS', 'DOMESTIC_TRANSFER', 'Transfer money personal to personal account. Always right to left.', '2016-01-01', '2016-01-01', true, 'scott');
+insert into bank.directory (dir_id, dir_group, dir_type, description, date_created, date_modified, is_active, user_id)
+values (12, 'OPERATIONS', 'REFILL', 'Transfer money from buffer account for refills to personal account.', '2016-01-01', '2016-01-01', true, 'scott');
+insert into bank.directory (dir_id, dir_group, dir_type, description, date_created, date_modified, is_active, user_id)
+values (13, 'OPERATIONS', 'WITHDRAWAL', 'Transfer money from personal account to buffer account for withdrawals.', '2016-01-01', '2016-01-01', true, 'scott');
+insert into bank.directory (dir_id, dir_group, dir_type, description, date_created, date_modified, is_active, user_id)
+values (14, 'OPERATIONS', 'VOID', 'This operation doing nothing.', '2016-01-01', '2016-01-01', true, 'scott');
+insert into bank.directory (dir_id, dir_group, dir_type, description, date_created, date_modified, is_active, user_id)
+values (15, 'ACCOUNTS', 'BUFFER', 'Buffer accounts for temporary keep money. Bank may contain more than one of buffer accounts.', '2016-01-01', '2016-01-01', true, 'scott');
+insert into bank.directory (dir_id, dir_group, dir_type, description, date_created, date_modified, is_active, user_id)
+values (16, 'ACCOUNTS', 'PERSONAL', 'Personal accounts for clients.', '2016-01-01', '2016-01-01', true, 'scott');
+insert into bank.directory (dir_id, dir_group, dir_type, description, date_created, date_modified, is_active, user_id)
+values (17, 'ACCOUNTS', 'INTEREST', 'Accounts can keep any kind of interest money. Bank may contain more than one of interest accounts.', '2016-01-01', '2016-01-01', true, 'scott');
+insert into bank.directory (dir_id, dir_group, dir_type, description, date_created, date_modified, is_active, user_id)
+values (18, 'ACCOUNTS', 'SYSTEM', 'Whole balance of the bank. Bank must containt only one system account.', '2016-01-01', '2016-01-01', true, 'scott');
+insert into bank.directory (dir_id, dir_group, dir_type, description, date_created, date_modified, is_active, user_id)
+values (19, 'PAPERS', 'PASSPORT', 'Passport', '2016-01-01', '2016-01-01', true, 'scott');
+insert into bank.directory (dir_id, dir_group, dir_type, description, date_created, date_modified, is_active, user_id)
+values (20, 'PAPERS', 'IDENTITY_CARD', 'Identity document', '2016-01-01', '2016-01-01', true, 'scott');
+insert into bank.directory (dir_id, dir_group, dir_type, description, date_created, date_modified, is_active, user_id)
+values (21, 'PAPERS', 'DRIVING_LICENCE', 'Driving licence', '2016-01-01', '2016-01-01', true, 'scott');
+alter sequence bank.directory_dir_id_seq restart with 22;
+
+
+insert into bank.customer_info values (0,	'MyBank',	'First',	'Branch',	'2015-12-31',	null,	true, 'scott',	'2016-01-02');
+insert into bank.customer_info values (1,	'Scott',	'Ridley',	'Alien',	'1937-11-30',	'2016-01-08',	true,	'scott',	'2016-01-02');
+alter sequence bank.customer_info_customer_id_seq restart with 2;
+
+
+insert into bank.customer_address values (1,	'USA, CA, San Andreas, 123',	'2016-01-02',	null,	true,	'scott',	7,	1);
+insert into bank.customer_address values (2,	'USA, NY, New York City, 123',	'2016-01-02',	null,	true,	'scott',	8,	1);
+alter sequence bank.customer_address_address_id_seq restart with 3;
+
+
+insert into bank.customer_contacts values (1,	'+18007573453256',	'2016-01-02',	null,	true,	'scott',	4,	1);
+insert into bank.customer_contacts values (2,	'scott@gmail.com',	'2016-01-02',	null,	true,	'scott',	5,	1);
+alter sequence bank.customer_contacts_contact_id_seq restart with 3;
+
+
+insert into bank.accounts values (1,	'0010101201600001016',	1,	'2016-01-01',	null,	'2016-01-02',	null,	'scott',	16,	false, null);
+insert into bank.accounts
+values (2,	'0010101201600002015',	0,	'2016-01-01',	null,	'2016-01-02',	null,	'scott',	15,	false, 'This account keeps money transferred from another banks to then transfer them to personal accounts.');
+insert into bank.accounts values (3,	'0010101201600003018',	0,	'2016-01-01',	null,	'2016-01-02',	null,	'scott',	18,	false, null);
+insert into bank.accounts values (4,	'0010101201600004017',	0,	'2016-01-01',	null,	'2016-01-02',	null,	'scott',	17,	false, null);
+insert into bank.accounts values (7,	'TEST12345',			1,	'2015-02-02',	null,	'2016-01-17',	null,	'scott',	16,	false, null);
+insert into bank.accounts values (8,	'TEST11111111111',		1,	'2016-01-13',	null,	'2016-01-17',	null,	'scott',	16,	false, null);
+insert into bank.accounts
+values (9, '0010101201600005015', 0, '2016-01-01', null, '2016-01-02', null, 'scott', 15, false, 'This account keeps money to then transfer them to another banks.');
+insert into bank.accounts
+values (10, '0010101201600006015', 0, '2016-01-01', null, '2016-01-02', null, 'scott', 15, false, 'This account temporarily keeps money deposited by client, until operator transfer them to clients personal account.');
+insert into bank.accounts
+values (11, '0010101201600007015', 0, '2016-01-01', null, '2016-01-02', null, 'scott', 15, false, 'This account temporarily keeps money transferred from client''s personal account by operator, until client physically gets money.');
+alter sequence bank.accounts_account_id_seq restart with 12;
+
+insert into bank.transactions values (0,	14,	false,	0,		'2016-01-02',	'19:10:41.554173',	'scott',	2,	3);
+insert into bank.transactions values (1,	12,	false,	2000,	'2016-01-02',	'18:42:14.941146',	'scott',	2,	1);
+insert into bank.transactions values (4,	14,	false,	0,		'2016-01-17',	'17:55:45.573993',	'scott',	2,	8);
+alter sequence bank.transactions_transaction_id_seq restart with 5;
+
+insert into bank.account_rest values (1,	1,	2000,		1,	'2016-01-02',	'18:46:51.652568');
+insert into bank.account_rest values (2,	2,	1000000,	0,	'2016-01-02',	'18:47:29.324624');
+insert into bank.account_rest values (3,	3,	1000000,	0,	'2016-01-02',	'18:47:42.796646');
+insert into bank.account_rest values (4,	4,	1000000,	0,	'2016-01-02',	'18:47:53.576661');
+insert into bank.account_rest values (5,	7,	0,			3,	'2016-01-17',	'14:50:20.239629');
+insert into bank.account_rest values (6,	8,	0,			4,	'2016-01-17',	'17:55:45.573993');
+insert into bank.account_rest values (7,	9,	1000000,	0,	'2016-01-02',	'18:47:29.324624');
+insert into bank.account_rest values (8,   10,	1000000,	0,	'2016-01-02',	'18:47:29.324624');
+insert into bank.account_rest values (9,   11,	1000000,	0,	'2016-01-02',	'18:47:29.324624');
+alter sequence bank.account_rest_rest_id_seq restart with 10;
---
+++ script/script.sql
@@ -0,0 +1,581 @@
+create database bankdb;
+create user scott with password 'tiger' superuser;
+create schema bank;
+
+CREATE TABLE bank.customer_info
+(
+  customer_id serial NOT NULL,
+  first_name character varying(64),
+  last_name character varying(64),
+  middle_name character varying(64),
+  birth_date date,
+  date_modified date,
+  is_active boolean,
+  user_id character varying(32),
+  date_created date,
+  CONSTRAINT customer_info_pk PRIMARY KEY (customer_id)
+);
+
+/* - Bank account structure: BBB DDMMYYYY CCCCC TTT (e.g. 0032812201500152056). BBB - branch, DDMMYYYY - open date, CCCCC - counter which start from 0 every business day, TTT - type of the account (buffer, personal, interest, system) */
+CREATE TABLE bank.accounts
+(
+  account_id serial NOT NULL,
+  account_number character varying(32),
+  account_owner integer,
+  date_opened date,
+  date_closed date,
+  date_created date,
+  date_modified date,
+  user_id character varying(32),
+  account_type integer,
+  is_suspended boolean,
+  comment character varying(4000),
+  CONSTRAINT accounts_pk PRIMARY KEY (account_id),
+  CONSTRAINT account_owner_fk FOREIGN KEY (account_owner)
+      REFERENCES bank.customer_info (customer_id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE CASCADE
+);
+
+/*
+- Domestic transactions. Domestic transaction can be fulfilled from one account (dropdown-list) to second.
+- Transactions between banks. If transaction is incoming, then first account should be foreign and second is domestic. If transaction is outgoing, then the first account is domestic and second is foreign.
+- Transactions between banks should use special one buffer account.
+- Transaction contains several types of operations: Money transfer (incoming, outgoing, between domestic accounts...always from right to left), Refill, Withdrawal
+*/
+CREATE TABLE bank.transactions
+(
+  transaction_id serial NOT NULL,
+  operation_type integer,
+  is_reversed boolean,
+  transaction_sum real,
+  transaction_date date,
+  transaction_time time without time zone,
+  user_id character varying(32),
+  account_debit integer,
+  account_credit integer,
+  CONSTRAINT transactions_pk PRIMARY KEY (transaction_id),
+  CONSTRAINT account_debit_fk FOREIGN KEY (account_debit)
+      REFERENCES bank.accounts (account_id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE CASCADE
+);
+
+CREATE TABLE bank.account_rest
+(
+  rest_id serial NOT NULL,
+  account_id integer,
+  rest_sum real,
+  transaction_id integer,
+  rest_date date,
+  rest_time time without time zone,
+  CONSTRAINT account_rest_pk PRIMARY KEY (rest_id),
+  CONSTRAINT account_rest_fk FOREIGN KEY (account_id)
+      REFERENCES bank.accounts (account_id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE CASCADE,
+  CONSTRAINT rest_transaction_fk FOREIGN KEY (transaction_id)
+      REFERENCES bank.transactions (transaction_id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE CASCADE
+);
+
+CREATE TABLE bank.bank_parameters
+(
+  parameter_id serial NOT NULL,
+  parent_id integer,
+  parameter_name character varying(64),
+  value character varying(1024),
+  date_created date,
+  date_modified date,
+  active_from date,
+  active_to date,
+  user_id character varying(32),
+  CONSTRAINT bank_parameters_pk PRIMARY KEY (parameter_id),
+  CONSTRAINT bank_parameters_fk FOREIGN KEY (parent_id)
+  REFERENCES bank.bank_parameters (parameter_id) MATCH SIMPLE
+  ON UPDATE NO ACTION ON DELETE NO ACTION
+);
+
+CREATE TABLE bank.customer_papers
+(
+  paper_id serial NOT NULL,
+  value character varying(1024),
+  date_crated date,
+  date_modified date,
+  is_active boolean,
+  user_id character varying(32),
+  paper_type integer,
+  customer_id integer,
+  CONSTRAINT customer_papers_pk PRIMARY KEY (paper_id),
+  CONSTRAINT customer_papers_fk FOREIGN KEY (customer_id)
+      REFERENCES bank.customer_info (customer_id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE CASCADE
+);
+
+CREATE TABLE bank.customer_address
+(
+  address_id serial NOT NULL,
+  value character varying(1024),
+  date_crated date,
+  date_modified date,
+  is_active boolean,
+  user_id character varying(32),
+  address_type integer,
+  customer_id integer,
+  CONSTRAINT customer_address_pk PRIMARY KEY (address_id),
+  CONSTRAINT customer_address_fk FOREIGN KEY (customer_id)
+      REFERENCES bank.customer_info (customer_id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE CASCADE
+);
+
+CREATE TABLE bank.customer_contacts
+(
+  contact_id serial NOT NULL,
+  value character varying(1024),
+  date_created date,
+  date_modified date,
+  is_active boolean,
+  user_id character varying(32),
+  contact_type integer,
+  customer_id integer,
+  CONSTRAINT customer_contacts_pk PRIMARY KEY (contact_id),
+  CONSTRAINT customer_contacts_fk FOREIGN KEY (customer_id)
+      REFERENCES bank.customer_info (customer_id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE CASCADE
+);
+
+CREATE TABLE bank.directory
+(
+  dir_id serial NOT NULL,
+  dir_group character varying(64),
+  dir_type character varying(64),
+  description character varying(1024),
+  date_created date,
+  date_modified date,
+  is_active boolean,
+  user_id character varying(32),
+  CONSTRAINT directory_pk PRIMARY KEY (dir_id)
+);
+
+CREATE OR REPLACE FUNCTION bank.change_customer_activity(
+    p_customer_id integer,
+    p_is_active boolean)
+  RETURNS boolean AS
+$BODY$
+begin
+   update bank.customer_info
+      set is_active     = p_is_active,
+          date_modified = now(),
+          user_id       = user
+    where customer_id = p_customer_id;
+
+   return true;
+	
+exception when others then 
+   return false;
+end; $BODY$
+  LANGUAGE plpgsql;
+  
+CREATE OR REPLACE FUNCTION bank.decrease_account_rest(
+    p_account integer,
+    p_transaction integer,
+    p_sum real)
+  RETURNS integer AS
+$BODY$
+declare
+   l_rest_id integer;
+   l_sum real;
+begin
+   select t.rest_sum 
+     into l_sum
+     from bank.account_rest t 
+    where t.account_id = p_account
+      and t.rest_id = (select max(z.rest_id)
+                         from bank.account_rest z
+                        where z.account_id = t.account_id);
+						
+   l_sum := l_sum - p_sum;
+						
+   insert into bank.account_rest (account_id, rest_sum, transaction_id, rest_date, rest_time)
+   values (p_account, l_sum, p_transaction, now(), now()) returning rest_id into l_rest_id;
+   
+   return l_rest_id;
+	
+exception when others then 
+   return -1;
+end; $BODY$
+  LANGUAGE plpgsql;
+  
+CREATE OR REPLACE FUNCTION bank.get_account_list(p_customer_id integer)
+  RETURNS refcursor AS
+$BODY$
+declare
+    rcur refcursor;
+begin
+    open rcur for select t.account_id,
+	                 t.account_number
+		    from bank.accounts t
+		   where t.account_owner = p_customer_id
+		     and now() between t.date_opened and coalesce(t.date_closed, now());
+    return rcur;
+end; $BODY$
+  LANGUAGE plpgsql;
+  
+CREATE OR REPLACE FUNCTION bank.get_account_rest(p_account integer)
+  RETURNS real AS
+$BODY$
+declare
+l_retval real := -1;
+begin
+   select t.rest_sum 
+     into l_retval
+     from bank.account_rest t 
+    where t.account_id = p_account
+      and t.rest_id = (select max(z.rest_id)
+                         from bank.account_rest z
+                        where z.account_id = t.account_id);
+   return l_retval;
+	
+exception when others then 
+   return -1;
+end; $BODY$
+  LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION bank.get_account_transactions(p_account_id integer)
+  RETURNS refcursor AS
+$BODY$
+declare
+    rcur refcursor;
+begin
+    open rcur for select transaction_id,
+			 (select z.dir_type
+			    from bank.directory z
+			   where z.is_active = true 
+			     and z.dir_id = t.operation_type) as operation_type,
+			 is_reversed,
+			 transaction_sum,
+			 transaction_date,
+			 to_char(transaction_time, 'hh24:mi:ss') as transaction_time,
+			 (select z.account_number
+			    from bank.accounts z
+			   where z.account_id = t.account_debit) as account_debit,
+			 (select z.account_number
+			    from bank.accounts z
+			   where z.account_id = t.account_credit) as account_credit
+		    from bank.transactions t
+		   where p_account_id in (t.account_debit, t.account_credit);
+    return rcur;
+end; $BODY$
+  LANGUAGE plpgsql;
+  
+CREATE OR REPLACE FUNCTION bank.get_account_types()
+  RETURNS refcursor AS
+$BODY$
+declare
+    rcur refcursor;
+begin
+    open rcur for select t.dir_id
+                       , t.dir_type
+		    from bank.directory t
+		   where t.is_active = true
+		     and t.dir_group = 'ACCOUNTS';
+    return rcur;
+end; $BODY$
+  LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION bank.get_address(p_customer_id integer)
+  RETURNS refcursor AS
+$BODY$
+declare
+    rcur refcursor;
+begin
+    open rcur for select ca.address_id
+		       , (select d.dir_type
+			    from bank.directory d
+			   where d.dir_group = 'ADDRESS'
+			     and d.dir_id = ca.address_type) as address_type
+		       , ca.value
+		    from bank.customer_address ca
+		   where ca.customer_id = p_customer_id
+		     and ca.is_active = true;
+   
+    return rcur;
+end; $BODY$
+  LANGUAGE plpgsql;
+  
+CREATE OR REPLACE FUNCTION bank.get_all_accounts()
+  RETURNS refcursor AS
+$BODY$
+declare
+    rcur refcursor;
+begin
+    open rcur for select t.account_id,
+	                 t.account_number
+		    from bank.accounts t
+		   where now() between t.date_opened and coalesce(t.date_closed, now());
+    return rcur;
+end; $BODY$
+  LANGUAGE plpgsql;
+  
+CREATE OR REPLACE FUNCTION bank.get_contacts(p_customer_id integer)
+  RETURNS refcursor AS
+$BODY$
+declare
+    rcur refcursor;
+begin
+    open rcur for select cc.contact_id
+		       , (select d.dir_type
+			    from bank.directory d
+			   where d.dir_group = 'CONTACTS'
+			     and d.dir_id = cc.contact_type) as contact_type
+		       , cc.value
+		    from bank.customer_contacts cc
+		   where cc.customer_id = p_customer_id
+		     and cc.is_active = true;
+   
+    return rcur;
+end; $BODY$
+  LANGUAGE plpgsql;
+  
+CREATE OR REPLACE FUNCTION bank.get_customer_accounts(p_customer_id integer)
+  RETURNS refcursor AS
+$BODY$
+declare
+    rcur refcursor;
+begin
+    open rcur for select t.account_id,
+                         t.account_number,
+                         t.account_owner,
+                         t.date_opened,
+                         coalesce(t.date_closed, '1901-01-01') as date_closed,
+                         t.date_created,
+                         coalesce(t.date_modified, '1901-01-01') as date_modified,
+                         t.user_id,
+                         (select z.dir_type
+                            from bank.directory z
+                           where z.dir_id = t.account_type) as account_type,
+                         t.is_suspended,
+                         (select z.rest_sum
+                            from bank.account_rest z
+                           where z.rest_id = (select max(r.rest_id)
+                                                from bank.account_rest r
+                                               where r.account_id = z.account_id)
+                             and z.account_id = t.account_id) as rest_sum
+                    from bank.accounts t
+                   where t.account_owner = p_customer_id
+                     and now() between t.date_opened and coalesce(t.date_closed, now());
+    return rcur;
+end; $BODY$
+  LANGUAGE plpgsql;
+  
+CREATE OR REPLACE FUNCTION bank.get_customer_info(p_pattern character varying)
+  RETURNS refcursor AS
+$BODY$
+declare
+    rcur refcursor;
+begin
+    open rcur for select t.customer_id,
+			 t.first_name,
+			 t.last_name,
+			 t.middle_name,
+			 t.birth_date,
+			 t.personal_id,
+			 t.is_resident,
+			 coalesce(t.date_modified, '1901-01-01') as date_modified,
+			 t.is_active,
+			 t.user_id,
+			 t.date_created
+                    from bank.customer_info t
+                   where t.first_name || t.last_name || t.middle_name like p_pattern
+                   order by t.customer_id;
+    return rcur;
+end; $BODY$
+  LANGUAGE plpgsql;
+  
+CREATE OR REPLACE FUNCTION bank.get_transaction_types()
+  RETURNS refcursor AS
+$BODY$
+declare
+    rcur refcursor;
+begin
+    open rcur for select t.dir_id
+                       , t.dir_type
+		    from bank.directory t
+		   where t.is_active = true
+		     and t.dir_group = 'OPERATIONS';
+    return rcur;
+end; $BODY$
+  LANGUAGE plpgsql;
+  
+CREATE OR REPLACE FUNCTION bank.increase_account_rest(
+    p_account integer,
+    p_transaction integer,
+    p_sum real)
+  RETURNS integer AS
+$BODY$
+declare
+   l_rest_id integer;
+   l_sum real;
+begin
+   select t.rest_sum 
+     into l_sum
+     from bank.account_rest t 
+    where t.account_id = p_account
+      and t.rest_id = (select max(z.rest_id)
+                         from bank.account_rest z
+                        where z.account_id = t.account_id);
+						
+   l_sum := l_sum + p_sum;
+						
+   insert into bank.account_rest (account_id, rest_sum, transaction_id, rest_date, rest_time)
+   values (p_account, l_sum, p_transaction, now(), now()) returning rest_id into l_rest_id;
+   
+   return l_rest_id;
+	
+exception when others then 
+   return -1;
+end; $BODY$
+  LANGUAGE plpgsql;
+  
+CREATE OR REPLACE FUNCTION bank.insert_account_info(
+    p_account_number character varying,
+    p_account_owner integer,
+    p_date_opened date,
+    p_account_type integer)
+  RETURNS boolean AS
+$BODY$
+declare
+l_account_id integer;
+l_transaction_id integer;
+begin
+   -- Adding new account
+   insert into bank.accounts (account_number, account_owner, date_opened, date_closed, date_created, date_modified, user_id, account_type, is_suspended)
+   values (p_account_number, p_account_owner, p_date_opened, null, now(), null, user, p_account_type, false) returning account_id into l_account_id;
+
+   -- Adding void transaction
+   insert into bank.transactions (operation_type, is_reversed, transaction_sum, transaction_date, transaction_time, user_id, account_debit, account_credit)
+   values (14, false, 0.0, now(), now(), user, 2, l_account_id) returning transaction_id into l_transaction_id;
+
+   -- Adding very first rest of theaccount and it is equal to 0
+   insert into bank.account_rest (account_id, rest_sum, transaction_id, rest_date, rest_time)
+   values (l_account_id, 0, l_transaction_id, now(), now());
+	
+   return true;
+	
+--exception when others then 
+  -- return false;
+end; $BODY$
+  LANGUAGE plpgsql;
+  
+CREATE OR REPLACE FUNCTION bank.insert_customer_info(
+    p_first_name character varying,
+    p_last_name character varying,
+    p_middle_name character varying,
+    p_birth_date date,
+    p_personal_id character varying,
+    p_is_resident boolean)
+  RETURNS boolean AS
+$BODY$
+begin
+   insert into bank.customer_info (first_name, last_name, middle_name, birth_date, personal_id, is_resident, date_created, date_modified, is_active, user_id)
+   values (p_first_name, p_last_name, p_middle_name, p_birth_date, p_personal_id, p_is_resident, now(), null, true, user);
+	
+   return true;
+	
+exception when others then 
+   return false;
+end; $BODY$
+  LANGUAGE plpgsql;
+  
+CREATE OR REPLACE FUNCTION bank.insert_transaction_info(
+    p_operation_type integer,
+    p_is_reversed boolean,
+    p_transaction_sum real,
+    p_account_debit integer,
+    p_account_credit integer)
+  RETURNS integer AS
+$BODY$
+declare
+l_rest_id integer;
+l_transaction_id integer;
+l_suspended boolean := false;
+k record;
+begin
+  
+  for k in (select t.is_suspended
+			  from bank.accounts t
+		     where t.account_id in (p_account_debit, p_account_credit))
+  loop
+    if k.is_suspended = true then
+	  l_suspended := true;
+    end if;
+  end loop;
+  
+  if l_suspended = false then
+  
+    if p_transaction_sum <= bank.get_account_rest(p_account_debit) then
+      insert into bank.transactions (operation_type, is_reversed, transaction_sum, transaction_date, transaction_time, user_id, account_debit, account_credit)
+        values (p_operation_type, p_is_reversed, p_transaction_sum, now(), now(), user, p_account_debit, p_account_credit) returning transaction_id into l_transaction_id;
+		  
+      l_rest_id := bank.decrease_account_rest(p_account_debit, l_transaction_id, p_transaction_sum);
+      if (l_rest_id >= 0) then 
+        l_rest_id := bank.increase_account_rest(p_account_credit, l_transaction_id, p_transaction_sum);
+      end if;
+    else 
+      return -2;
+    end if;
+
+  else
+    return -3;
+  end if;
+
+  return l_rest_id;	
+exception when others then 
+   return -1;
+end; $BODY$
+  LANGUAGE plpgsql;
+  
+CREATE OR REPLACE FUNCTION bank.update_account_info(
+    p_account_id integer,
+    p_is_suspended boolean)
+  RETURNS boolean AS
+$BODY$
+begin
+   update bank.accounts
+      set is_suspended   = p_is_suspended,
+          date_modified  = now(),
+          user_id        = user
+    where account_id     = p_account_id;
+
+   return true;
+	
+exception when others then 
+   return false;
+end; $BODY$
+  LANGUAGE plpgsql;
+  
+CREATE OR REPLACE FUNCTION bank.update_customer_info(
+    p_customer_id integer,
+    p_first_name character varying,
+    p_last_name character varying,
+    p_middle_name character varying,
+    p_birth_date date,
+    p_personal_id character varying,
+    p_is_resident boolean)
+  RETURNS boolean AS
+$BODY$
+begin
+   update bank.customer_info
+      set first_name    = p_first_name,
+          last_name     = p_last_name,
+          middle_name   = p_middle_name,
+          birth_date    = p_birth_date,
+          personal_id   = p_personal_id,
+          is_resident   = p_is_resident,
+          date_modified = now(),
+          user_id       = user
+    where customer_id = p_customer_id;
+
+   return true;
+	
+exception when others then 
+   return false;
+end; $BODY$
+  LANGUAGE plpgsql;
